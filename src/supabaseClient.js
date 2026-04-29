aef75293fe5b4236c4efc11da1e6160b84e83213
mazen <mazenmohamed3910@gmail.com>
2026-04-29 22:04:41 +0300

    second commit

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://pfpdakjkuzennwaizypr.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmcGRha2prdXplbm53YWl6eXByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0MTExOTAsImV4cCI6MjA5Mjk4NzE5MH0.fl-WC9A6P6oLewvW_fUBHX-jt78pbi9UrnA2rsmktYk";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
